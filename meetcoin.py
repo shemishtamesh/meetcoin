989cc5f029c11cdaef23391d39e08599b9edc652
shemishtamesh <62944862+shemishtamesh@users.noreply.github.com>
2021-02-07 13:02:01 +0200

    finished is_valid() for Transaction

--- meetcoin.py
+++ meetcoin.py
@@ -8,6 +8,9 @@ from datetime import datetime
 
 CURVE_FOR_KEYS = 'NIST P-256'
 STANDARD_FOR_SIGNATURES = 'fips-186-3'
+STAKE_ADDRESS = "STAKE_ADDRESS"
+TRANSACTION_FEE = 1
+EXPORT_IMPORT_KEY_FORMAT = 'PEM'
 
 
 def sha256_hash(*args):
@@ -32,15 +35,15 @@ class Transaction:
         self.sender = sender # sender's wallet's public key
         self.receiver = receiver # receiver's wallet's public key
         self.amount = amount
-        self.fee = 0
+        self.fee = TRANSACTION_FEE
 
     def is_valid(self, blockchain):
         """returns true iff the transaction is valid"""
         # check signature against sender and rest of transaction:
         hash_of_transaction = self.hash_transaction()
-        verifier = DSS.new(self.sender, STANDARD_FOR_SIGNATURES)
+        verifier = DSS.new(ECC.import_key(self.sender), STANDARD_FOR_SIGNATURES)
         try:
-            verifier.verify(hash_of_transaction, self.signature)
+            verifier.verify(hash_of_transaction, eval(self.signature))
         except ValueError:
             return False
 
@@ -53,29 +56,37 @@ class Transaction:
         if not re.match(regex_for_uuids, str(self.id)):
             return False
 
-        # check if the receiver is valid:
-        p = 115792089210356248762697446949407573530086143415290314195533631308867097853951
-        b = 41058363725152142129326129780047268409114441015993725554835256314039467401291
-        a = -3
+        # check if the receiver and sender are valid (if it's a point on the elliptic curve):
+        # is receiver valid:
+        if self.receiver != STAKE_ADDRESS:
+            receiver_key = ECC.import_key(self.receiver)
+            p = 115792089210356248762697446949407573530086143415290314195533631308867097853951
+            b = 41058363725152142129326129780047268409114441015993725554835256314039467401291
+            a = -3
+            try:
+                x = int(receiver_key.pointQ.x)
+                y = int(receiver_key.pointQ.y)
+            except AttributeError:
+                return False
+            if ((y ** 2) - ((x ** 3) - (a * x) + b)) % p == 0:
+                return False
+
+        # is sender valid:
+        sender_key = ECC.import_key(self.sender)
         try:
-            x = int(self.receiver.pointQ.x)
-            y = int(self.receiver.pointQ.y)
+            x = int(sender_key.pointQ.x)
+            y = int(sender_key.pointQ.y)
         except AttributeError:
             return False
         if ((y ** 2) - ((x ** 3) - (a * x) + b)) % p == 0:
             return False
 
-        # check if sender address is valid:
-        try:
-            x = int(self.sender.pointQ.x)
-            y = int(self.sender.pointQ.y)
-        except AttributeError:
-            return False
-        if ((y ** 2) - ((x ** 3) - (a * x) + b)) % p == 0:
+        if self.receiver == STAKE_ADDRESS and self.amount <= 0: # check if valid for stake transaction:
             return False
 
         # check if fee is valid:
-        pass  # TODO: change this (hadn't decided on a fee system yet)
+        if int(self.fee) != TRANSACTION_FEE:
+            return False
 
         return True
 
@@ -84,7 +95,7 @@ class Transaction:
                + f"time: {self.time}\n" \
                + f"signature: {self.signature}\n" \
                + f"sender: {self.sender}\n" \
-               + f"reveiver: {self.receiver}\n" \
+               + f"receiver: {self.receiver}\n" \
                + f"amount: {self.amount}\n" \
                + f"fee: {self.fee}\n"
 
@@ -93,6 +104,7 @@ class Transaction:
 
 
 class Block:
+    # TODO: change block.time to block.number
     def __init__(self,
                  time="0000-00-00 00:00:00.000000",
                  prev_hash=None,
@@ -115,7 +127,7 @@ class Block:
 
     def hash_block(self):
         """returns the hash of the block"""
-        # return sha256_hash(self.time, self.prev_hash, self.data, self.validator, self.signature)
+        # return sha256_hash(self.time, self.prev_hash, self.data, self.validator, self.signature) #TODO: decide if uncomment this
         return sha256_hash(self.time, self.prev_hash, self.data)
 
     def is_valid(self):
@@ -168,34 +180,56 @@ class Blockchain:
     def get_balance(self, public_key):
         ret_value = 0
         for block in self.chain:
-            if block.validator == public_key:
-                ret_value += block.fee
-
             for transaction in block.data:
-                if transaction.receiver == public_key:
-                    ret_value += (transaction.amount - block.fee)
+                if transaction.receiver != STAKE_ADDRESS:
+                    if block.validator == public_key:
+                        ret_value += transaction.fee
 
-                elif transaction.sender == public_key:
-                    ret_value -= (transaction.amount - block.fee)
+                    if transaction.receiver == public_key:
+                        ret_value += (transaction.amount + transaction.fee)
+
+                if transaction.sender == public_key:
+                    ret_value -= (transaction.amount + transaction.fee)
 
         return ret_value
+
+    def get_validators(self):
+        """returns a dict of all addresses that have coins staked and how many coins they have staked"""
+        validators = {}
+        for block in self.chain:
+            for transaction in block.data:
+                if transaction.receiver == STAKE_ADDRESS:
+                    if not (transaction.sender in validators):
+                        validators[transaction.sender] = transaction.amount
+                    else:
+                        validators[transaction.sender] += transaction.amount
+
+        return validators
+
+    def get_leader(self):
+        validators = self.get_validators()
+        return max(validators.items(), key=(lambda key: validators[key])) #TODO: change this to a better ellection system
 
 
 class Wallet:
     def __init__(self, secret_key=ECC.generate(curve=CURVE_FOR_KEYS)):
         self.secret_key = secret_key # secret key = private key
         self.public_key = self.secret_key.public_key()
-        self.blockchain = Blockchain() # TODO: change this to object Blockchain
+        self.blockchain = Blockchain()
+        self.transaction_pool = []
+
+    def hash_for_block_signature(self, block):
+        return sha256_hash(block.time, block.prev_hash, block.data, self.public_key)
 
     def make_transaction(self, receiver, amount):
-        """gets a receiver (public key) and an amount, returns a transaction"""
-        id = uuid.uuid4()
-        time = datetime.now()
-        sender = self.public_key
-        fee = 0  # TODO: change this to the decided fee system
+        """gets a receiver (exported public key) and an amount (int), returns a transaction (Transaction)"""
+        id = str(uuid.uuid4())
+        time = str(datetime.now())
+        sender = self.public_key.export_key(format=EXPORT_IMPORT_KEY_FORMAT)
+        fee = str(TRANSACTION_FEE)
         transaction_hash = sha256_hash(id, time, sender, receiver, amount, fee)
         signer = DSS.new(self.secret_key, STANDARD_FOR_SIGNATURES)
-        signature = signer.sign(transaction_hash)
+        signature = str(signer.sign(transaction_hash))
         return Transaction(receiver, sender, amount, signature, id, time)
 
     def __str__(self):
@@ -205,7 +239,14 @@ class Wallet:
 
 
 def main():
-    pass
+    w=Wallet()
+    w1=Wallet()
+    t=w.make_transaction(str(w1.public_key.export_key(format=EXPORT_IMPORT_KEY_FORMAT)), 1)
+    # for var in vars(t):
+    #     print(f"{var} - {type(var)}")
+    # print(t.signature)
+    print(t.is_valid())
 
 
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+    main()
